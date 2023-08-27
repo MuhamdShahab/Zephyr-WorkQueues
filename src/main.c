f0a22be5aeff1b86a64c1d4433ec3b854c08bed7
Shahab <muhammadshahab.business@gmail.com>
2023-08-28 04:02:20 +0500

    work Queues

--- src/main.c
+++ src/main.c
@@ -1,31 +1,37 @@
+/*
+
+Title: Work Queue
+Source: https://docs.zephyrproject.org/latest/kernel/services/threads/workqueue.html
+
+
+*/
+
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
 
-#define STACKSIZE 1024
-#define THREAD0_PRIORITY 7
-#define THREAD1_PRIORITY 7
+#define OUR_QUEUE_SIZE  1024
+#define OUR_QUEUE_PRIORITY 5
 
+K_THREAD_STACK_DEFINE(our_queue_area, OUR_QUEUE_SIZE); 
 
-void thread0(void)
+struct k_work_q our_work_q; 
+
+struct k_work our_work;
+
+static void our_work_fn(struct k_work *work)
 {
-    while (1)
-    {
-        printk("Hello, I am thread0\n");
-        k_msleep(500);
-
-    }
+    printk("I am the first task in the queue.");
 }
 
-void thread1(void)
+
+void main(void)
 {
-    while (1)
-    {
-        printk("Hello, I am thread1\n");
-        k_msleep(500);
 
-    }
+    k_work_queue_init(&our_work_q);
+
+    k_work_queue_start(&our_work_q, our_queue_area, K_THREAD_STACK_SIZEOF(our_queue_area), OUR_QUEUE_PRIORITY, NULL);
+
+    k_work_init(&our_work, our_work_fn);
+
+    k_work_submit_to_queue(&our_work_q, &our_work);
+
 }
-
-K_THREAD_DEFINE(thread0_id, STACKSIZE, thread0, NULL, NULL, NULL, THREAD0_PRIORITY, 0, 0);
-K_THREAD_DEFINE(thread1_id, STACKSIZE, thread1, NULL, NULL, NULL, THREAD1_PRIORITY, 0, 0);
-
